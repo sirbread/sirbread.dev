222f5bb0f8503913e18669f5d7b8d931ee80822f
sirbread <gverma0824@outlook.com>
2025-02-08 20:01:37 -0700

    Delete startMenu.js

--- startMenu.js
+++
@@ -1,8 +0,0 @@
-function toggleStartMenu() {
-    var startMenu = document.getElementById('startMenu');
-    if (startMenu.style.display === 'block') {
-        startMenu.style.display = 'none';
-    } else {
-        startMenu.style.display = 'block';
-    }
-}
